da9581680802128134682e670fd3385fe6acd519
William Chang <wilicw1346@gmail.com>
2020-02-13 17:04:41 +0800

    remove semicolons

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
-const apiRouter = require('./routes/api');
+const express = require('express')
+const path = require('path')
+const indexRouter = require('./routes/index')
+const apiRouter = require('./routes/api')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/api', apiRouter);
+app.use('/', indexRouter)
+app.use('/api', apiRouter)
 
-module.exports = app;
+module.exports = app
