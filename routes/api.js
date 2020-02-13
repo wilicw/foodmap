50f9bbf7fdbece9f7102fa6ad0ac2d53cd3fb5b6
William Chang <wilicw1346@gmail.com>
2020-02-13 16:56:56 +0800

    connect the db first

--- routes/api.js
+++ routes/api.js
@@ -5,34 +5,34 @@ const MongoClient = require('mongodb').MongoClient;
 const DB_URL = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`;
 const client = new MongoClient(DB_URL);
 
-router.get('/', function(req, res, next) {
+client.connect(err => {
+  console.log(err);
+});
+
+/* GET nothing */
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
+/* GET stores list */
 router.get('/stores', (req, res, next) => {
-  client.connect(err => {
-    const stores = client.db(process.env.DB).collection("stores").find({});
-    stores.toArray((err, docs) => {
-      console.log(docs)
-      res.send(JSON.stringify(docs));
-    })
-  });
+  const stores = client.db(process.env.DB).collection("stores").find({});
+  stores.toArray((err, docs) => {
+    res.send(JSON.stringify(docs));
+  })
 });
 
+/* GET food types */
 router.get('/types', (req, res, next) => {
-  client.connect(err => {
-    const stores = client.db(process.env.DB).collection("stores").find({});
-    stores.toArray((err, docs) => {
-      let types = []
-      for (var index in docs) {
-        console.log(docs[index])
-        types.push(docs[index].type)
-      }
-      types = [... new Set(types.flat())]
-      res.send(JSON.stringify(types));
-    })
-  });
+  const stores = client.db(process.env.DB).collection("stores").find({});
+  stores.toArray((err, docs) => {
+    let types = []
+    for (var index in docs) {
+      types.push(docs[index].type)
+    }
+    types = [... new Set(types.flat())]
+    res.send(JSON.stringify(types));
+  })
 });
-
 
 module.exports = router;
