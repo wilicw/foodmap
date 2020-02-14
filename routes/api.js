55e5222355d9376a850bcb6805a0baa827b39190
William Chang <wilicw1346@gmail.com>
2020-02-14 09:38:14 +0800

    score api

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 require('dotenv').config()
 
+const sanitize = require('mongo-sanitize')
+const ObjectId = require('mongodb').ObjectID
 const MongoClient = require('mongodb').MongoClient
 const DB_URL = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`
 const client = new MongoClient(DB_URL)
@@ -34,6 +36,40 @@ router.get('/types', (req, res, next) => {
     types = [... new Set(types.flat())]
     res.send(JSON.stringify(types))
   })
+})
+
+/* POST uploads socre */
+router.post('/score', (req, res, next) => {
+  const data = req.body
+  const now = new Date().getTime()
+  const store_id = sanitize(data.store)
+  const user = sanitize(data.user)
+  const score = sanitize(data.score)  
+  let stores = client.db(process.env.DB).collection("stores")
+  
+  stores.updateOne(
+    {_id: new ObjectId(store_id)},
+    {
+      $pull: {
+        scores: {
+          user : user
+        }
+      }
+    }
+  )
+  stores.updateOne(
+    {_id: new ObjectId(store_id)},
+    {
+      $addToSet: {
+        scores: {
+          user : user,
+          score : score,
+          timestamp : now
+        }
+      }
+    }
+  )
+  res.send("200")
 })
 
 module.exports = router
